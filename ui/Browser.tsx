76295b6e5006a72cd7a7c0b02ccfcd148edbe2fb
Valentin Manceaux-Panot <va.mcxp@gmail.com>
2022-11-28 20:21:06 +0100

    Formatted Browser

--- ui/Browser.tsx
+++ ui/Browser.tsx
@@ -1,16 +1,16 @@
 import * as styles from './Browser.css'
 
 const Browser = () => (
-    <div className={styles.topBar}>
-        <div className={styles.buttons}>
-            <div className={styles.redButtons}></div>
-            <div className={styles.yellowButtons} ></div>
-            <div className={styles.greenButtons}></div>
-            <div className={styles.url}>
-                <p className={styles.paragraph}>http://www.design-system.com</p>
-            </div>
-        </div>
+  <div className={styles.topBar}>
+    <div className={styles.buttons}>
+      <div className={styles.redButtons}></div>
+      <div className={styles.yellowButtons}></div>
+      <div className={styles.greenButtons}></div>
+      <div className={styles.url}>
+        <p className={styles.paragraph}>https://design-system.com</p>
+      </div>
     </div>
+  </div>
 )
-     
+
 export default Browser
